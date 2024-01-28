3544e372a50d334a5378de87e217955bcbf8762c
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-01-28 23:28:12 +0400

    page strukturuna elage ve haqqimizda sehifeleri elave edildi.

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,8 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 'config.global_context_processors.global_todo_categories_context',
+                'config.global_context_processors.global_page_context',
+                
             ],
         },
     },
